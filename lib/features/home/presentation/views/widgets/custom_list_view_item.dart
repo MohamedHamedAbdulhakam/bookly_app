8495f65c63592d0b236816e6942c1e09ed6078a2
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-08-22 23:31:29 +0300

    features list view item prat three

---
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/assets.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: AspectRatio(
+        aspectRatio: 3 / 5,
+        child: Container(
+          width: 100,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.red,
+              image: const DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(AssetsDate.testImage),
+              )),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -3,6 +3,8 @@ import 'package:bookly_app/features/home/presentation/views/widgets/custom_appba
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'custom_list_view_item.dart';
+
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
@@ -10,24 +12,6 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [CustomAppBar(), CustomListViewItem()],
-    );
-  }
-}
-
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 50,
-      decoration: const BoxDecoration(
-          color: Colors.red,
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: AssetImage(AssetsDate.testImage),
-          )),
     );
   }
 }
