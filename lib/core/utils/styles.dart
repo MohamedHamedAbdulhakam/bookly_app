2117fb13501928da33bcfe0a86f81b2f1bd32171
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-08-23 12:48:52 +0300

    add style files and text to home

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class Styles {
+  static const titileMudium = TextStyle(
+      color: Colors.white,
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal);
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,9 +1,11 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'custom_list_view_item.dart';
+import 'featured_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -11,28 +13,21 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomAppBar(),
-        FeaturedBooksListView(),
+        Padding(
+          padding: EdgeInsets.only(left: 10),
+          child: FeaturedBooksListView(),
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Text('Best Seller', style: Styles.titileMudium),
+        )
       ],
-    );
-  }
-}
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-        itemCount: 10, // Number of items in the list
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const CustomListViewItem();
-        },
-      ),
     );
   }
 }
