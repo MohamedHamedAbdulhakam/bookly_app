7a6b6880673e5bd4144aec968dd49162b55bdd71
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-08-23 15:52:24 +0300

    add font family

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -3,9 +3,10 @@ import 'package:google_fonts/google_fonts.dart';
 
 abstract class Styles {
   static const titileMudium = TextStyle(
-      color: Colors.white,
-      fontSize: 20,
-      fontWeight: FontWeight.w600,
-      decoration: TextDecoration.none,
-      fontStyle: FontStyle.normal);
+    color: Colors.white,
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    decoration: TextDecoration.none,
+    // fontStyle: FontStyle.normal););
+  );
 }
