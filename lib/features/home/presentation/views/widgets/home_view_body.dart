a36996022a7f7c36e5238c971ce1a0fa9cbd25a6
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-08-23 22:32:41 +0300

    best seller item  part two

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'featured_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -44,18 +45,28 @@ class BestSellerListViewItem extends StatelessWidget {
           padding: const EdgeInsets.only(left: 18),
           child: SizedBox(
             height: 100,
-            child: AspectRatio(
-              aspectRatio: 3 / 5,
-              child: Container(
-                width: 100,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: Colors.red,
-                    image: const DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage(AssetsDate.testImage),
-                    )),
-              ),
+            child: Row(
+              children: [
+                AspectRatio(
+                  aspectRatio: 3 / 5,
+                  child: Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: Colors.red,
+                        image: const DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage(AssetsDate.testImage),
+                        )),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Text('Harry Potter and the globlet of fire',
+                        style: Styles.textStyle20.copyWith()),
+                  ],
+                )
+              ],
             ),
           ),
         ),
