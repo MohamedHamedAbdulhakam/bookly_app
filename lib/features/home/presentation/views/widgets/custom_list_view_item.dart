e651cc286038514a0126d393ff20216c16e2720f
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-08-23 11:56:54 +0300

    featured list view

--- lib/features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -7,8 +7,8 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
       child: AspectRatio(
         aspectRatio: 3 / 5,
         child: Container(
