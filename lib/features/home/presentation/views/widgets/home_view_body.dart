0cbaea0cec3e45ab377599b1c7f1d3d7dc502f20
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-08-23 16:39:59 +0300

    best seller list view item part one

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -2,9 +2,6 @@ import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-import 'custom_list_view_item.dart';
 import 'featured_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -26,7 +23,42 @@ class HomeViewBody extends StatelessWidget {
         Padding(
           padding: EdgeInsets.only(left: 20),
           child: Text('Best Seller', style: Styles.titileMudium),
-        )
+        ),
+        BestSellerListViewItem(),
+      ],
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 18),
+          child: SizedBox(
+            height: 100,
+            child: AspectRatio(
+              aspectRatio: 3 / 5,
+              child: Container(
+                width: 100,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.red,
+                    image: const DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(AssetsDate.testImage),
+                    )),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
