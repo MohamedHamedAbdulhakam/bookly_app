374e435f67ecc80503248739ad8d33d024b86dc0
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-08-29 20:59:05 +0300

    add rating to book detail

--- lib/features/home/presentation/views/widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -3,31 +3,30 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.start});
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Row(
-        children: [
-          const Icon(
-            FontAwesomeIcons.solidStar,
-            color: Color(0xffFFDD4F),
-          ),
-          const SizedBox(
-            width: 6.3,
-          ),
-          const Text(
-            '4.8',
-            style: Styles.textStyle16,
-          ),
-          const SizedBox(width: 5),
-          Text(
-            '(246)',
-            style: Styles.textStyle14,
-          )
-        ],
-      ),
+    return Row(
+      mainAxisAlignment: mainAxisAlignment,
+      children: [
+        const Icon(
+          FontAwesomeIcons.solidStar,
+          color: Color(0xffFFDD4F),
+          size: 16,
+        ),
+        const SizedBox(
+          width: 6.3,
+        ),
+        Text('4.8',
+            style: Styles.textStyle16.copyWith(fontWeight: FontWeight.bold)),
+        const SizedBox(width: 5),
+        Text(
+          '(246)',
+          style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+        )
+      ],
     );
   }
 }
