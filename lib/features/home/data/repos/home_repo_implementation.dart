1654b2230bacda3e6f0fdfb28f41c1c573739480
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-08-31 21:28:14 +0300

    implement fech featured books methods

--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -29,8 +29,21 @@ class HomeRepoImplementation implements HomeRepo {
   }
 
   @override
-  Future<Either<Failures, List<BookModel>>> fechFeaturedBooks() {
-    // TODO: implement fechFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fechFeaturedBooks() async {
+    try {
+      var data = await apiServices.get(
+          endpoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+      // TODO
+    }
   }
 }
