a71b57d651f4223825cd514ddb30b3ff656529b5
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-08-31 15:55:15 +0300

    either class

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,9 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fechBestSellerBooks(); //withot implementation because i used an absract calss
-  fechFeaturedBooks();
+  Future<Either<Failures, List<BookModel>>>
+      fechBestSellerBooks(); //withot implementation because i used an absract calss
+  Future<Either<Failures, List<BookModel>>> fechFeaturedBooks();
 }
